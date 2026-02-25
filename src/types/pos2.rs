9770378069044264426f69e39e9bb8dec3d1dada
catthatmeows <zairysbigtae@gmail.com>
2026-02-25 14:56:58 +0800

    operations

--- src/types/pos2.rs
+++ src/types/pos2.rs
@@ -1,5 +1,6 @@
 use std::ops::{Add, Sub, Mul, Div};
 use crate::types::vec2::Vec2;
+use num_traits::{Float, ToPrimitive};
 
 #[derive(Debug, Clone, Copy)]
 pub enum Pos2 {
@@ -38,6 +39,40 @@ impl Sub for Pos2 {
             (Pos2::Relative(a), Pos2::Relative(b)) => Pos2::Relative(a - b),
             (Pos2::Absolute(a), Pos2::Relative(b)) => Pos2::Absolute(a - b),
             (Pos2::Relative(a), Pos2::Absolute(b)) => Pos2::Absolute(a - b),
+        }
+    }
+}
+
+/// Allow adding a generic Vec2<T> to a Pos2 by converting the Vec2<T> to Vec2<f32>.
+/// T must be convertible to f32 via `ToPrimitive` and meet the same bounds that
+/// make `Vec2<T>::to_f32()` available.
+impl<T> Add<Vec2<T>> for Pos2
+where
+    T: Float + ToPrimitive + Copy + Add<Output = T>,
+{
+    type Output = Self;
+
+    fn add(self, other: Vec2<T>) -> Self::Output {
+        let o = other.to_f32();
+        match self {
+            Pos2::Absolute(a) => Pos2::Absolute(a + o),
+            Pos2::Relative(a) => Pos2::Relative(a + o),
+        }
+    }
+}
+
+/// Allow subtracting a generic Vec2<T> from a Pos2 by converting the Vec2<T> to Vec2<f32>.
+impl<T> Sub<Vec2<T>> for Pos2
+where
+    T: Float + ToPrimitive + Copy + Add<Output = T>,
+{
+    type Output = Self;
+
+    fn sub(self, other: Vec2<T>) -> Self::Output {
+        let o = other.to_f32();
+        match self {
+            Pos2::Absolute(a) => Pos2::Absolute(a - o),
+            Pos2::Relative(a) => Pos2::Relative(a - o),
         }
     }
 }
